0aa3fccef20e0eb8573c41a42074c0e1acbd62c1
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-06-24 10:04:08 +0300

    Add sector col to analyses table
    user's sector can be different than their chosen questionnaire

---
+++ database/migrations/2023_06_24_064108_add_sector_col_to_analyses_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Database\Helpers\MigHelper as MH;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('analyses', function (Blueprint $table) {
+            MH::ref($table, 'sector_id', false, true, 'questionnaires');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('analyses', function (Blueprint $table) {
+            $table->dropColumn('sector_id');
+        });
+    }
+};

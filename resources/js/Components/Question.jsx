cf60979e53f6f2dc2ef01e8885b6c25c1f701024
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-06-26 13:32:03 +0300

    Refactor: clean up Question component

--- resources/js/Components/Question.jsx
+++ resources/js/Components/Question.jsx
@@ -1,9 +1,9 @@
 const Question = ({ question, answers }) => {
     return (
         <div className="grid grid-cols-1 gap-1">
-            <label htmlFor={`q${question.id}`} className="grid grid-cols-1 gap-1">
+            <label className="grid grid-cols-1 gap-1">
                 <div className="text-lg text-gray-700" dangerouslySetInnerHTML={{__html: question.question}} />
-                <div className="text-base italic text-gray-500" dangerouslySetInnerHTML={{__html: question.note}} />
+                {question.note ? (<div className="text-base italic text-gray-500" dangerouslySetInnerHTML={{__html: question.note}} />) : null}
             </label>
             <div children={answers.map(a => (
                 <div className="radio" key={`q${question.id}a${a.id}`} >

67287bd059219517de3efe86bfb24ec1cfa052be
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-06-30 09:35:59 +0300

    Improve Input component
    - add special styling and better support for checkboxes
    - add props: defaultValue, autoComplete, placeholder
    - add error viewing support

--- resources/js/Components/Input.jsx
+++ resources/js/Components/Input.jsx
@@ -1,12 +1,19 @@
-const Input = ({ name, label, options = {}, type = "text", required = false }) => {
+const Input = ({ name, label, options = {}, type = "text", required = false, defaultValue = null, autoComplete = null, placeholder = null, error = null }) => {
     return (
-        <div className="flex flex-col-reverse gap-1">
+        <div className={`flex ${type == "checkbox" ? "flex-row items-center gap-2" : "flex-col-reverse gap-1"}`}>
+            {error && (<p className="text-sm pp-fg-red">{error}</p>)}
             {options.length > 0 ? (
-                <select name={name} required={required} defaultValue={required ? null : "N/A"} children={
+                <select name={name} id={name} required={required} defaultValue={defaultValue ?? (required ? null : "N/A")} children={
                     options.map(v => (<option key={`${name}-${v}`} value={v}>{v}</option>))
                             .concat(required ? [] : [<option key={`${name}-N/A`} value="N/A">N/A</option>])
                 }/>
-            ) : (<input type={type} name={name} id={name} required={required}/>)}
+            ) : (
+                type == "checkbox" ? (
+                    <input type="checkbox" name={name} id={name} checked={defaultValue}/>
+                ) : (
+                    <input type={type} name={name} id={name} defaultValue={defaultValue} autoComplete={autoComplete} placeholder={placeholder} required={required}/>
+                )
+            )}
             <label htmlFor={name} className="pp-text">{label}</label>
         </div>
     );

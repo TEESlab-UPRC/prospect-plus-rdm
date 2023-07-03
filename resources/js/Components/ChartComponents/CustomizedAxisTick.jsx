589ccfc159f2dd5d217e156abf6d7555f09e2653
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-03 19:57:23 +0300

    Refactor

--- resources/js/Components/ChartComponents/CustomizedAxisTick.jsx
+++ resources/js/Components/ChartComponents/CustomizedAxisTick.jsx
@@ -1,5 +1,9 @@
 import { Text } from 'recharts';
 
-const CustomizedAxisTick = ({ isYAxis, x, y, payload, style, className }) => (<Text x={x} y={y} className={className} style={style} width={isYAxis ? 60 : 95} textAnchor={isYAxis ? "end" : "middle"} verticalAnchor={isYAxis ? "middle" : "start"}>{payload.value}</Text>);
+const CustomizedAxisTick = ({ isYAxis, x, y, payload, style, className }) => (
+    <Text x={x} y={y} className={className} style={style} width={isYAxis ? 60 : 155} textAnchor={isYAxis ? "end" : "middle"} verticalAnchor={isYAxis ? "middle" : "start"}>
+        {payload.value}
+    </Text>
+);
 
 export default CustomizedAxisTick;

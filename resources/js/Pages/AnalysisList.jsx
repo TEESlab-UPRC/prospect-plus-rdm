0a3f0f2b00db50c66a6f9285b2dd2fb5e5a01b3b
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-03 18:31:00 +0300

    Add button for new analysis @ analysis list

--- resources/js/Pages/AnalysisList.jsx
+++ resources/js/Pages/AnalysisList.jsx
@@ -1,9 +1,13 @@
 import Analyses from '@/Components/Analyses';
 import Layout from '@/Layouts/GeneralLayout';
+import { router } from '@inertiajs/react';
 
 export default function AnalysisList({ auth, analyses }) {
+    const goToHome = () => router.get(route('home.render'));
+
     return (
         <Layout title="Analysis List" auth={auth}>
+            <button className="pp-btn-yellow" onClick={goToHome}>Create a new analysis</button>
             <Analyses analyses={analyses}/>
         </Layout>
     );

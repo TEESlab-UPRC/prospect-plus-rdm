0f042d4279814865bb3be9b263db17efbd5c7f76
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-04 12:13:46 +0300

    Improve new analysis button looks @ analysis list

--- resources/js/Pages/AnalysisList.jsx
+++ resources/js/Pages/AnalysisList.jsx
@@ -1,13 +1,22 @@
 import Analyses from '@/Components/Analyses';
 import Layout from '@/Layouts/GeneralLayout';
 import { router } from '@inertiajs/react';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 export default function AnalysisList({ auth, analyses }) {
     const goToHome = () => router.get(route('home.render'));
 
+    const NewAnalysisBtn = () => (
+        <span>
+            <button className="pp-btn-yellow" onClick={goToHome}>
+                <AddCircleOutlineIcon />Create new analysis
+            </button>
+        </span>
+    );
+
     return (
         <Layout title="Analysis List" auth={auth}>
-            <button className="pp-btn-yellow" onClick={goToHome}>Create a new analysis</button>
+            <NewAnalysisBtn />
             <Analyses analyses={analyses}/>
         </Layout>
     );

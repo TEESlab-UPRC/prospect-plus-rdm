fad4bd480207e1ce81eeab14bc11539e5e9478c4
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-05 00:13:18 +0300

    Refactor

--- resources/js/Layouts/GeneralLayout.jsx
+++ resources/js/Layouts/GeneralLayout.jsx
@@ -6,20 +6,20 @@ import Footer from '@/Components/Footer';
 
 const altLayoutRoutes = ['welcome', 'login', 'register'];
 
+const PoweredBy = () => (
+    <div className="powered-by-logo">
+        <span>Powered by</span>
+        <TEESlab />
+    </div>
+);
+
 export default function Layout({ className, title, auth, children }) {
     const isAltLayout = altLayoutRoutes.includes(route().current());
 
     return (    // TODO: add footer
         <>
             <Head title={title}/>
-            {isAltLayout ? (
-                <div className="powered-by-logo">
-                    <span>Powered by</span>
-                    <TEESlab />
-                </div>
-            ) : (
-                <MenuBar user={auth.user}/>
-            )}
+            {isAltLayout ? (<PoweredBy />) : (<MenuBar user={auth.user}/>)}
             <div className="pp-outer-container">
                 <div className="pp-middle-container">
                     <div className={`pp-inner-container ${className}`}>

2d9cd565c2fda7328d3510083399f122736ebba0
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-05 19:35:05 +0300

    Add background image

--- resources/img/misc/MiscImg.js
+++ resources/img/misc/MiscImg.js
@@ -1,7 +1,9 @@
 import MenuArrow from './menu-arrow.svg';
 import EuropeanFlag from './European-flag.png';
+import Background from './Background-Compressed.jpg';
 
 export default {
     MenuArrow,
-    EuropeanFlag
+    EuropeanFlag,
+    Background
 };

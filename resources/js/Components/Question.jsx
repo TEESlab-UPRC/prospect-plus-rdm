c80b5023e98ad425e05f7dd059fe2279baf2a503
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-05 13:32:46 +0300

    Cleanup

--- resources/js/Components/Question.jsx
+++ resources/js/Components/Question.jsx
@@ -6,7 +6,7 @@ const Question = ({ question, answers, num }) => {
                 {question.note ? (<div className="text-base italic text-gray-500" dangerouslySetInnerHTML={{__html: question.note}} />) : null}
             </label>
             <div children={answers.map(a => (
-                <div className="radio" key={`q${question.id}a${a.id}`} >
+                <div key={`q${question.id}a${a.id}`} >
                     <label className="pp-input">
                         <input type="radio" className="pp-input" name={`q${question.id}`} value={a.value} /> {/* TODO make answers required */}
                         <span className="ml-3 text-base italic font-semibold text-gray-700">{a.answer}</span>

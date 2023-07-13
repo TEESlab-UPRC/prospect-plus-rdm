7cb5902f6bfbc69380cbd9d499c79f5e2959a413
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-13 21:02:55 +0300

    Add direct support for useForm to Input component

--- resources/js/Components/Input.jsx
+++ resources/js/Components/Input.jsx
@@ -1,21 +1,28 @@
-const Input = ({ name, label = null, options = {}, type = "text", defaultValue = null, checked = null, error = null, errorObj = {}, status = null, ...props }) => {
+const Input = ({ name, label = null, options = {}, type = "text", defaultValue = null, checked = null, error = null, errorObj = {}, status = null, formObj = {}, onChange = null, ...props }) => {
+    errorObj = formObj.errors ?? errorObj;
     error = error ?? errorObj[name];
+    defaultValue = defaultValue ?? (formObj.data ? formObj.data[name] : null);
     checked = checked ?? defaultValue;
+
+    var onChangeD = e => {
+        onChange && onChange(e);
+        formObj.setData && formObj.setData(name, e.target.value);
+    };
 
     return (
         <div className={`flex ${type == "checkbox" ? "flex-row items-center gap-2 pp-checkbox" : "flex-col-reverse gap-1 pp-input"}`}>
             {status && (<p className="text-sm pp-fg-green">{status}</p>)}
             {error && (<p className="text-sm pp-fg-red">{error}</p>)}
             {options.length > 0 ? (
-                <select {...props} name={name} id={name} defaultValue={defaultValue ?? (required ? null : "N/A")} children={
+                <select {...props} name={name} id={name} defaultValue={defaultValue ?? (required ? null : "N/A")} onChange={onChangeD} children={
                     options.map(v => (<option key={`${name}-${v}`} value={v}>{v}</option>))
                             .concat(required ? [] : [<option key={`${name}-N/A`} value="N/A">N/A</option>])
                 }/>
             ) : (
                 type == "checkbox" ? (
-                    <input {...props} type="checkbox" name={name} id={name} checked={checked}/>
+                    <input {...props} type="checkbox" name={name} id={name} checked={checked} onChange={onChangeD}/>
                 ) : (
-                    <input {...props} type={type} name={name} id={name} defaultValue={defaultValue}/>
+                    <input {...props} type={type} name={name} id={name} defaultValue={defaultValue} onChange={onChangeD}/>
                 )
             )}
             {label && (<label htmlFor={name} className="pp-text">{label}</label>)}

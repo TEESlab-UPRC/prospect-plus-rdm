884e8e59fde739b5d748696ffd77e4b1407eaa7e
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-13 21:05:49 +0300

    Update ResetPassword page to use custom code

--- resources/js/Pages/Auth/ResetPassword.jsx
+++ resources/js/Pages/Auth/ResetPassword.jsx
@@ -1,12 +1,9 @@
 import { useEffect } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, useForm } from '@inertiajs/react';
+import Input from '@/Components/Input';
+import { useForm } from '@inertiajs/react';
+import Layout from '@/Layouts/GeneralLayout';
 
-export default function ResetPassword({ token, email }) {
+export default function ResetPassword({ auth, token, email }) {
     const { data, setData, post, processing, errors, reset } = useForm({
         token: token,
         email: email,
@@ -22,69 +19,19 @@ export default function ResetPassword({ token, email }) {
 
     const submit = (e) => {
         e.preventDefault();
-
         post(route('password.store'));
     };
 
     return (
-        <GuestLayout>
-            <Head title="Reset Password" />
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        isFocused={true}
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
-
-                    <TextInput
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password_confirmation', e.target.value)}
-                    />
-
-                    <InputError message={errors.password_confirmation} className="mt-2" />
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Reset Password
-                    </PrimaryButton>
+        <Layout title="Reset Password" auth={auth} className="max-w-2xl">
+            <form onSubmit={submit} className="flex flex-col gap-6">
+                <Input name="email" label="Email" formObj={{data, setData, errors}} autoComplete="username" type="email" required disabled/>
+                <Input name="password" label="Password" formObj={{data, setData, errors}} autoComplete="new-password" type="password" required/>
+                <Input name="password_confirmation" label="Confirm Password" formObj={{data, setData, errors}} autoComplete="new-password" type="password" required/>
+                <div className="flex items-center justify-end">
+                    <button type="submit" className="pp-btn-blue" disabled={processing}>Reset Password</button>
                 </div>
             </form>
-        </GuestLayout>
+        </Layout>
     );
 }

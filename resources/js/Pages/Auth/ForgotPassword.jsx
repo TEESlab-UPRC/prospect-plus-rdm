5bc93238d71d80f94f3ed29a643de6c19a3132e2
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-13 21:05:35 +0300

    Update ForgotPassword page to use custom code

--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -1,50 +1,29 @@
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, useForm } from '@inertiajs/react';
+import Input from '@/Components/Input';
+import { useForm } from '@inertiajs/react';
+import Layout from '@/Layouts/GeneralLayout';
 
-export default function ForgotPassword({ status }) {
+export default function ForgotPassword({ auth, status }) {
     const { data, setData, post, processing, errors } = useForm({
         email: '',
     });
 
     const submit = (e) => {
         e.preventDefault();
-
         post(route('password.email'));
     };
 
     return (
-        <GuestLayout>
-            <Head title="Forgot Password" />
-
-            <div className="mb-4 text-sm text-gray-600">
+        <Layout title="Forgot Password" auth={auth} className="max-w-2xl gap-7">
+            <div className="pp-text">
                 Forgot your password? No problem. Just let us know your email address and we will email you a password
                 reset link that will allow you to choose a new one.
             </div>
-
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
-
-            <form onSubmit={submit}>
-                <TextInput
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={data.email}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    onChange={(e) => setData('email', e.target.value)}
-                />
-
-                <InputError message={errors.email} className="mt-2" />
-
-                <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Email Password Reset Link
-                    </PrimaryButton>
+            <form onSubmit={submit} className="flex flex-col gap-10">
+                <Input name="email" label="Email" formObj={{data, setData, errors}} status={status} autoComplete="username" type="email" required/>
+                <div className="flex items-center justify-end">
+                    <button type="submit" className="pp-btn-blue" disabled={processing}>Email Password Reset Link</button>
                 </div>
             </form>
-        </GuestLayout>
+        </Layout>
     );
 }

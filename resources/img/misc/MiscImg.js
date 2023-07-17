61e73a2658ee982d655031567bcb247b0ff37e8d
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-17 20:10:26 +0300

    Add bg img license/source

--- resources/img/misc/MiscImg.js
+++ resources/img/misc/MiscImg.js
@@ -1,6 +1,6 @@
 import MenuArrow from './menu-arrow.svg';
 import EuropeanFlag from './European-flag.png';
-import Background from './Background-Compressed.jpg';
+import Background from './Background-Compressed.jpg';   // CC0, https://www.b4place.com/cities/#bwg9/6590
 import Favicon from './favicon.ico';
 
 export default {

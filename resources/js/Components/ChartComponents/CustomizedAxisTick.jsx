8ae77ebbdf5dacbdcee9d7e6a588641094f14ac3
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-17 04:39:52 +0300

    Add offset support to CustomizedAxisTick

--- resources/js/Components/ChartComponents/CustomizedAxisTick.jsx
+++ resources/js/Components/ChartComponents/CustomizedAxisTick.jsx
@@ -1,9 +1,14 @@
 import { Text } from 'recharts';
 
-const CustomizedAxisTick = ({ isYAxis, x, y, payload, style, className }) => (
-    <Text x={x} y={y} className={className} style={style} width={isYAxis ? 60 : 155} textAnchor={isYAxis ? "end" : "middle"} verticalAnchor={isYAxis ? "middle" : "start"}>
-        {payload.value}
-    </Text>
-);
+const CustomizedAxisTick = ({ isYAxis, x, y, payload, style, className, offset = 0 }) => {
+    if(isYAxis) x -= offset;
+    else y += offset;
+
+    return (
+        <Text x={x} y={y} className={className} style={style} width={isYAxis ? 60 : 155} textAnchor={isYAxis ? "end" : "middle"} verticalAnchor={isYAxis ? "middle" : "start"}>
+            {payload.value}
+        </Text>
+    );
+};
 
 export default CustomizedAxisTick;

e420452c3c1b6529e7eb4f2dc47a26809bf73063
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-20 01:01:51 +0300

    Improve DialogHelpers
    - add support for custom width
    - add support for html body

--- resources/js/Helpers/DialogHelpers.js
+++ resources/js/Helpers/DialogHelpers.js
@@ -1,27 +1,36 @@
 import Swal from 'sweetalert2'
 import { getCSSVar } from './DomHelpers';
 
-export const confirm = (title, message, confirmCallback, cancelCallback = null, finallyCallback = null) => Swal.fire({
-    title: title,
-    text: message,
-    showCancelButton: true,
-    reverseButtons: true,
-    cancelButtonText: "No",
-    confirmButtonText: "Yes",
-    confirmButtonColor: getCSSVar("pp-green")
-}).then(res => {
+export const confirm = (title, message, confirmCallback, cancelCallback = null, finallyCallback = null, width = null, htmlMsg = null) => Swal.fire(Object.assign(
+    {
+        title: title,
+        width: width ?? "32rem",
+        showCancelButton: true,
+        reverseButtons: true,
+        cancelButtonText: "No",
+        confirmButtonText: "Yes",
+        confirmButtonColor: getCSSVar("pp-green")
+    },
+    message == null ? {html: htmlMsg} : {text: message}
+)).then(res => {
     if(res.isConfirmed) confirmCallback();
     else if(cancelCallback) cancelCallback(res.dismiss);
     if(finallyCallback) finallyCallback();
 });
 
-export const info = (title, message, callback = null) => Swal.fire({
-    title: title,
-    text: message,
-    confirmButtonColor: getCSSVar("pp-cyan")
-}).then(() => callback && callback());
+export const info = (title, message, callback = null, width = null, htmlMsg = null) => Swal.fire(Object.assign(
+    {
+        title: title,
+        width: width ?? "32rem",
+        confirmButtonColor: getCSSVar("pp-cyan")
+    },
+    message == null ? {html: htmlMsg} : {text: message}
+)).then(() => callback && callback());
+
+export const htmlInfo = (title, htmlMsg, callback = null, width = null) => info(title, null, callback, width, htmlMsg);
 
 export default {
     confirm,
-    info
+    info,
+    htmlInfo
 };

a86dfb815ea0ab361f397e79b9b9ffe77385b029
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-20 01:02:59 +0300

    Implement Help & Support message

--- resources/js/Components/Menu.jsx
+++ resources/js/Components/Menu.jsx
@@ -1,8 +1,21 @@
 import Dropdown from '@/Components/Dropdown';
 import MiscImg from '@/../img/misc/MiscImg';
+import { htmlInfo } from '@/Helpers/DialogHelpers';
+import Websites from '@/Constants/Websites';
+
+const showHelp = e => {
+    e.preventDefault();                                                             // abort link navigation
+    e.target.parentElement.dispatchEvent(new MouseEvent("click", {bubbles: true})); // close menu
+    htmlInfo("Help & Support", `
+        <div class="flex flex-col gap-2 my-1">
+            <span><b>Need help using the app?</b> Check out the <a ${Websites.Guidelines ? `href="${Websites.Guidelines}"` : ""} target="_blank" class="pp-link">User Guide</a>!</span>
+            <span><b>Need further support or to contact us?</b> Email us: rdmtool.teeslab [at] unipi.gr</span>
+        </div>
+    `, null, "45rem");
+};
 
 const Menu = ({ user }) => {
-    const Link = ({ dest, text, ...props }) => (<Dropdown.Link {...props} href={route(dest)}>{text}</Dropdown.Link>);
+    const Link = ({ dest = null, text, onClick, ...props }) => (<Dropdown.Link {...props} href={dest ? route(dest) : null} onClick={onClick}>{text}</Dropdown.Link>);
     const PLink = ({ dest, text }) => (<Link dest={dest} text={text} method="post" as="button"/>);
 
     return (
@@ -18,7 +31,7 @@ const Menu = ({ user }) => {
                 <Dropdown.Content>
                     <Link dest="home.render" text="Home"/>
                     {user && (<Link dest="analyses.render" text="My Analyses"/>)}
-                    <Link dest="home.render" text="Help & Support"/>{/* TODO: add page for this placeholder */}
+                    <Link onClick={showHelp} text="Help & Support" as="button"/>
                     {user && (<Link dest="profile.render" text="Edit Profile"/>)}
                     {user ? (<PLink dest="logout" text="Log Out"/>) : (<Link dest="login" text="Log In"/>)}
                 </Dropdown.Content>

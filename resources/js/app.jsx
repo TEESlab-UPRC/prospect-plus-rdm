c69e7311a14588d362108365a1778b72b2d001fb
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-07-23 02:48:43 +0300

    Change progress bar color to fit theme

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,6 +5,7 @@ import '../css/app.css';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { getCSSVar } from './Helpers/DomHelpers';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -13,6 +14,6 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup: ({ el, App, props }) => createRoot(el).render(<App {...props} />),
     progress: {
-        color: '#4B5563',
+        color: getCSSVar("pp-cyan"),
     },
 });

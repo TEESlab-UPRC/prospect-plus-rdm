c342e557d77d5b56009fe27393de2e49d96e86c3
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-08-19 10:01:12 +0300

    Make sector titles @ sector selection translatable

--- resources/img/sectors/SectorImg.js
+++ resources/img/sectors/SectorImg.js
@@ -1,8 +1,8 @@
-import PublicBuildings from './1-public-buildings.png';
-import PrivateBuildings from './2-private-buildings.png';
-import Transport from './3-transport.png';
-import PublicLighting from './4-public-lighting.png';
-import CrossSectoral from './5-cross-sectoral.png';
+import PublicBuildings from './1-public-buildings-no-text.png';
+import PrivateBuildings from './2-private-buildings-no-text.png';
+import Transport from './3-transport-no-text.png';
+import PublicLighting from './4-public-lighting-no-text.png';
+import CrossSectoral from './5-cross-sectoral-no-text.png';
 
 export default {
     PublicBuildings,
--- resources/js/Pages/SectorSelection.jsx
+++ resources/js/Pages/SectorSelection.jsx
@@ -1,16 +1,55 @@
 import SectorImg from '@/../img/sectors/SectorImg';
 import PostLink from '@/Components/PostLink';
+import { onPageLoad } from '@/Helpers/DomHelpers';
 import useTransHelper from '@/Helpers/TransHelpers';
 import Layout from '@/Layouts/GeneralLayout';
+import { useEffect } from 'react';
 
-const ImgLink = ({img, dest}) => (
-    <PostLink href={route('questionnaire.load')} data={{type: 'rdm', title: dest}} child={
-        <img src={img}/>
-    }/>
-);
+const postAnimFrame = callback => window.requestAnimationFrame(() => setTimeout(callback, 0));
+
+const ratiosAreClose = (f1, f2) => Math.abs(f1 - f2) < 0.0001;
+
+const transSTc = (c, pWR = 10, pHR = 10) => {
+    const cB = c.getBoundingClientRect();
+    const t = c.children[0];
+    const cW = cB.width, cH = cB.height;
+    const tW = t.clientWidth, tH = t.clientHeight;
+    const wR = cW / tW, hR = cH / tH;
+    t.style.transform = `translateY(-50%) scale(${Math.min(wR, hR)})`;
+    if(!(ratiosAreClose(pWR, wR) && ratiosAreClose(pHR, hR)))
+        postAnimFrame(() => transSTc(c, wR, hR));
+}
+
+const transST = root => Array.from(root.getElementsByClassName("sector-title-container")).forEach(c => transSTc(c));
 
 export default function SectorSelection({ auth, env, locale }) {
     const { t } = useTransHelper();
+    var animFrame = null;
+    var contW = -1;
+
+    useEffect(() => onPageLoad(() => {
+        const cont = document.body.getElementsByClassName("sector-select")[0];
+        contW = cont.getBoundingClientRect().width;
+        document.body.onresize = () => {
+            const cW = cont.getBoundingClientRect().width;
+            if(contW == cW) return;
+            contW = cW;
+            if(animFrame){
+                cancelAnimationFrame(animFrame);
+                animFrame = null;
+            }
+            animFrame = postAnimFrame(() => transST(document.body));
+        };
+    }), []);
+
+    const ImgLink = ({img, dest}) => (
+        <PostLink href={route('questionnaire.load')} data={{type: 'rdm', title: dest}} child={
+            <>
+                <img src={img} onLoad={e => transST(e.target.parentElement)}/>
+                <div className="sector-title-container"><span>{t(dest)}</span></div>
+            </>
+        }/>
+    );
 
     return (
         <Layout title="Sector Selection" auth={auth} env={env} locale={locale} className="text-center">

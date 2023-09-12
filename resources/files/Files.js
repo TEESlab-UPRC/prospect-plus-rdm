423ec79188f5fdb0c9ff43c29b9d9c44ddfe9e74
Alex811 <22715818+811Alex@users.noreply.github.com>
2023-09-13 02:57:45 +0300

    Add guidelines PDF

---
+++ resources/files/Files.js
@@ -0,0 +1,5 @@
+import Guidelines from './PROSPECT+_D5.7–Guidebook_for_the_Upgraded_Decision-Making_Matrix.pdf';
+
+export default {
+    Guidelines
+};
--- resources/js/Constants/Websites.js
+++ resources/js/Constants/Websites.js
@@ -1,6 +1,8 @@
+import Files from "@/../files/Files"
+
 export const GAOptOutAddon = "https://tools.google.com/dlpage/gaoptout/";
 export const Repo = "https://github.com/TEESlab-UPRC/prospect-plus-rdm";
-export const Guidelines = null;   // TODO: put guidelines link here
+export const Guidelines = Files.Guidelines;
 export const TEESlab = "https://teeslab.unipi.gr/";
 export const PROSPECTplus = "https://h2020prospect.eu/";
 export const PPLearningHandbooks = "https://h2020prospect.eu/learning-handbooks";

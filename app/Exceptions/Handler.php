3368d0827068a4a7dc0814c923dbd035571f6373
Alex811 <22715818+811Alex@users.noreply.github.com>
2024-01-11 21:45:31 +0200

    Redirect to login page on TokenMismatchException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +27,9 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+        $this->renderable(function (Exception $e) {
+            if($e->getPrevious() instanceof TokenMismatchException) return to_route('login');
         });
     }
 }
